3dc8da3d59e3c56b22cbab502e37b9de13c9b125
Quinn Dougherty <quinn.dougherty.forall@gmail.com>
2025-05-05 16:43:17 -0400

    State of the imp fork as inherited from prior efforts

--- flake.nix
+++ flake.nix
@@ -4,14 +4,31 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, parts }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      parts,
+    }@inputs:
     parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "x86_64-linux" ];
-      perSystem = { pkgs, ... }: {
-        devShells.default = let
-          name = "Formal Containment dev";
-          buildInputs = with pkgs; [ elan uv typst typstyle nixfmt-rfc-style ];
-        in pkgs.mkShell { inherit name buildInputs; };
-      };
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default =
+            let
+              name = "Formal Containment dev";
+              buildInputs = with pkgs; [
+                elan
+                uv
+                typst
+                typstyle
+              ];
+            in
+            pkgs.mkShell { inherit name buildInputs; };
+        };
     };
 }
--- imp/Imp.lean
+++ imp/Imp.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Imp` library.
 -- Import modules here that should be built as part of the library.
-import Imp.Basic
+import Imp.Expr
+import Imp.Stmt
