8370b67c1e305c5075e319aa7bd305daccd66f3e
Quinn Dougherty <quinn.dougherty.forall@gmail.com>
2025-06-02 08:17:48 -0400

    Configure pantograph repl

--- flake.nix
+++ flake.nix
@@ -3,35 +3,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     parts.url = "github:hercules-ci/flake-parts";
+    pantograph.url = "github:lenianiva/Pantograph";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      parts,
-    }@inputs:
+  outputs = { self, nixpkgs, parts, pantograph, }@inputs:
     parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "x86_64-linux"
-      ];
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells.default =
-            let
-              name = "Formal Containment dev";
-              buildInputs = with pkgs; [
-                elan
-                uv
-                typst
-                typstyle
-                nodejs_24
-                lefthook
-                pandoc
-              ];
-            in
-            pkgs.mkShell { inherit name buildInputs; };
-        };
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = let
+          name = "Formal Containment dev";
+          buildInputs = with pkgs; [
+            elan
+            uv
+            typst
+            typstyle
+            nodejs_24
+            lefthook
+            pandoc
+            pantograph.packages.${pkgs.system}.executable
+          ];
+        in pkgs.mkShell { inherit name buildInputs; };
+      };
     };
 }
